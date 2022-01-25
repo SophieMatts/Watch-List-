7c26f3a0d77de7be53fbc0be6259010b2b33c975
sophie mattacks <s.mattacks@hotmail.co.uk>
2022-01-25 17:59:31 +0000

    Views Updated and Stylesheets Added

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
+# Rails.application.routes.draw do
+#   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+#   resources :lists do
+#     resources :bookmarks
+#   end
+#   delete "bookmarks/:id", to: "bookmarks#destroy"
+#   # Defines the root path route ("/")
+#   # root "articles#index"
+# end
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :lists do
-    resources :bookmarks
+  root to: 'lists#index'
+  resources :lists, except: [:edit, :update] do
+    resources :bookmarks, only: [:new, :create]
+    resources :reviews, only: :create
   end
-  delete "bookmarks/:id", to: "bookmarks#destroy"
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :bookmarks, only: :destroy
+  resources :reviews, only: :destroy
 end
