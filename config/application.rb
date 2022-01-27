79a906e4f9ab9c999c1e3e4ae3f194a5e953aef8
sophie mattacks <s.mattacks@hotmail.co.uk>
2022-01-27 20:51:18 +0000

    Watch List - Heroku

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ require "action_text/engine"
 require "action_view/railtie"
 require "action_cable/engine"
 # require "rails/test_unit/railtie"
-config.assets.initialize_on_precompile = false
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -23,6 +23,7 @@ module RailsWatchList
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.assets.initialize_on_precompile = false
 
     # Configuration for the application, engines, and railties goes here.
     #
